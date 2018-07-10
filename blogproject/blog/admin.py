70cedbf311009d7f6ec2da43b1ef773847584ffa
yulekeji <1171936142@qq.com>
2018-07-10 20:01:58 +0800

    init

---
+++ blogproject/blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Post, Category, Tag
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
